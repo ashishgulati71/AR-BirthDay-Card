b2c210c8b7fc163269178d316fcd60d6f2229a08
ashishgulati71 <ashishgulati71@gmail.com>
2017-05-02 23:54:22 +0530

    generic

---
+++ Assets/_Custom/blowflame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blowflame : MonoBehaviour {
+	public GameObject game;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void blowing () {
+		game.SetActive(false);
+	}
+	public void back(){
+		Application.LoadLevel (0);
+		game.SetActive(true);
+	}
+}
